f7f509af5bdf3480b59145c4157619d37c62d33a
Andramonova92 <bakaeva92@list.ru>
2016-01-22 23:10:43 +0200

    ArchiveProject 22.01.16

--- ArchiveProject/ArchiveProject/MainForm.cs
+++ ArchiveProject/ArchiveProject/MainForm.cs
@@ -21,40 +21,42 @@ namespace ArchiveProject
 
         private void create_archive_Click(object sender, EventArgs e)
         {
-            FileSystemWatcher watcher = new FileSystemWatcher();
-            watcher.Path = "D:\\";
-            watcher.Filter = "*txt";
-            watcher.EnableRaisingEvents = true;
-            List <object> files = new List<object>{"1.txt", "2.txt"};
-           
+            List<object> files = new List<object> { "D:\\1.txt", "D:\\2.txt" };
             string rar = "D:\\rar";
             FileInfo r = new FileInfo(rar); // создаем экземпляр класса FileInfo для использования в потоке filerar
-            //FileInfo f = new FileInfo();
+           
             FileStream filerar = null;
-            try
-            {
-              // byte[] array = files.SelectMany(s => s.ToString())
-               //.Select(Convert.ToByte)
-              // .ToArray(); // в масив байтов считываем все байты файла для архива
+            
+                try
+                {
+                    // byte[] array = files.SelectMany(s => s.ToString())
+                    //.Select(Convert.ToByte)
+                    // .ToArray(); // в масив байтов считываем все байты файла для архива
 
-                MemoryStream memory = new MemoryStream(); // создаем обьект MemoryStream в который помещаем масив байтов
-                filerar = r.Create(); // создаем файл архива
-                BinaryFormatter bf = new BinaryFormatter();
-                bf.Serialize(memory, files);
-                memory.WriteTo(filerar); // копируем сорержимое мемори в поток
+                    // MemoryStream memory = new MemoryStream(); // создаем обьект MemoryStream в который помещаем масив байтов
+                    filerar = r.Create(); // создаем файл архива
+                    foreach (object file in files)
+                    {
+                        string f = Path.GetFileName(file.ToString());
+                        BinaryFormatter bf = new BinaryFormatter();
+                        bf.Serialize(filerar, f);
+                    }
+                    
+                    // memory.WriteTo(filerar); // копируем сорержимое мемори в поток
 
-            }
-            catch
-            {
+                }
+                catch
+                {
 
-                MessageBox.Show("ошибка архивации");
+                    MessageBox.Show("ошибка архивации");
 
-            }
-            finally
-            {
-                if (rar != null) // если файл не пустой
-                    filerar.Close(); // закрываем поток
+                }
+                finally
+                {
+                    if (rar != null) // если файл не пустой
+                        filerar.Close(); // закрываем поток
+                }
             }
         }
     }
-}
+
